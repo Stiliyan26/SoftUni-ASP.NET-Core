92814f8984f1681d3a28d0eb90cbf918599640ec
Stiliyan26 <stili262002@abv.bg>
2023-04-12 21:39:13 +0300

    initial commit

---
+++ WebShopDemo/WebShopDemo.Core/Contracts/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShopDemo.Core.Models;
+
+namespace WebShopDemo.Core.Contracts
+{
+    /// <summary>
+    /// Manipulates product
+    /// </summary>
+    public interface IProductService
+    {
+        /// <summary>
+        /// Gets all products
+        /// </summary>
+        /// <returns>List of products</returns>
+        Task<IEnumerable<ProductDto>> GetAll();
+    }
+}
---
+++ WebShopDemo/WebShopDemo.Core/Models/ProductDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShopDemo.Core.Models
+{
+    /// <summary>
+    /// Product model
+    /// </summary>
+    public class ProductDto
+    {
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Prodcut name
+        /// </summary>
+       
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Product price
+        /// </summary>
+        [Range(typeof(decimal), "0.1", "1000")]
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Available quantity
+        /// </summary>
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
