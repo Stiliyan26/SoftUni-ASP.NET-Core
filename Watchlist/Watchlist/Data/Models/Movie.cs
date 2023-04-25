2c09986ee4aaa9db14b56e7ae563a872db05203e
Stiliyan26 <stili262002@abv.bg>
2023-04-25 13:25:12 +0300

    Watchlist init

---
+++ Watchlist/Watchlist/Data/Models/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+
+namespace Watchlist.Data.Models
+{
+    public class Movie
+    {
+        public Movie()
+        {
+            UsersMovies = new List<UserMovie>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(50)]
+        public string Director { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Genre))]
+        public int GenreId { get; set; }
+
+        [Required]
+        public Genre Genre { get; set;} = null!;
+
+        public List<UserMovie> UsersMovies { get; set; }
+    }
+}
