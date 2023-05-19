0790afd7ea02e5d23cb2e2a9ef58ee76921e7952
Stiliyan26 <stili262002@abv.bg>
2023-05-19 23:33:25 +0300

    options added

---
+++ ConsoleApp1/ConsoleApp1/Task/Machine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Machine
+    {
+        protected Machine(string name, int yearOfCreation)
+        {
+            Name = name;
+            YearOfCreation = yearOfCreation;
+        }
+
+        public string Name { get; set; }
+
+        public int YearOfCreation { get; set; }
+
+        public abstract void MakeSound();
+
+        public abstract void Greet(string personToGreet);
+    }
+}
--- HouseRentingSysteм/HouseRentingSysteм/Program.cs
+++ HouseRentingSysteм/HouseRentingSysteм/Program.cs
@@ -9,8 +9,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => 
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+    options.Password.RequiredLength = 4;
+    options.Password.RequireNonAlphanumeric = false;
+})
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
