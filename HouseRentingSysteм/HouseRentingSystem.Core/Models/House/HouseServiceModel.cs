b2053cf0d73edde1c79df8720a92d5b2fa2dec54
Stiliyan26 <stili262002@abv.bg>
2023-06-11 13:30:08 +0300

    all method implemented!

---
+++ HouseRentingSysteм/HouseRentingSystem.Core/Models/House/HouseServiceModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HouseRentingSystem.Core.Models.House
+{
+    public class HouseServiceModel
+    {
+        public int Id { get; init; }
+
+        public string Title { get; init; } = null!;
+
+        public string Address { get; init; } = null!;
+
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; init; } = null!;
+
+        [Display(Name = "Price per month")]
+        public decimal PricePerMonth { get; init; }
+
+        [Display(Name = "Is Rented")]
+        public bool IsRented { get; init; }
+    }
+}
--- HouseRentingSysteм/HouseRentingSystem.Core/Models/House/HousesQueryModel.cs
+++ HouseRentingSysteм/HouseRentingSystem.Core/Models/House/HousesQueryModel.cs
@@ -8,6 +8,8 @@ namespace HouseRentingSystem.Core.Models.House
 {
     public class HousesQueryModel
     {
+        public int TotalHousesCount { get; set; }
 
+        public IEnumerable<HouseServiceModel> Houses { get; set; } = new List<HouseServiceModel>();
     }
 }
