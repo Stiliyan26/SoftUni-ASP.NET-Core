1ef6ce03b78d7f3cc1cf202819652c991a36dfc2
Stiliyan26 <stili262002@abv.bg>
2023-06-14 13:34:35 +0300

    Contacts - Register implemeneted

---
+++ SoftUniTeams Exam/Contacts/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using Contacts.Data.Models;
+using Contacts.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts.Controllers
+{
+    [AllowAnonymous]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public UserController(
+            UserManager<ApplicationUser> _userManager,
+            SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RegisterViewModel model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+            };
+
+            var result = await userManager.CreateAsync(user);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            RegisterViewModel model = new RegisterViewModel();
+
+            return View(model);
+        }
+    }
+}
---
+++ SoftUniTeams Exam/Contacts/Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contacts.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [StringLength(60, MinimumLength = 10)]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
