f5ceae454705e725856208014f6a094aa83de241
Stiliyan26 <stili262002@abv.bg>
2023-06-14 13:13:49 +0300

    Contacts - database init

---
+++ SoftUniTeams Exam/Contacts/Data/ContactsDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Contacts.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Data
+{
+    public class ContactsDbContext : IdentityDbContext
+    {
+        public ContactsDbContext(DbContextOptions<ContactsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        public DbSet<ApplicationUserContact> ApplicationUsersContacts { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUserContact>()
+                .HasKey(uc => new { uc.ApplicationUserId, uc.ContactId });
+
+            builder
+               .Entity<Contact>()
+               .HasData(new Contact()
+               {
+                   Id = 1,
+                   FirstName = "Bruce",
+                   LastName = "Wayne",
+                   PhoneNumber = "+359881223344",
+                   Address = "Gotham City",
+                   Email = "imbatman@batman.com",
+                   Website = "www.batman.com"
+               });
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
